a4dc928da57b9ea64d9b950774947210940ddce5
Erione White <erionewhite1999@gmail.com>
2023-05-25 16:51:27 -0500

    trying again

--- inkling/settings.py
+++ inkling/settings.py
@@ -96,9 +96,7 @@ CORS_ALLOWED_ORIGINS = [
     'http://erionew.github.io'
 ]
 
-CORS_ALLOWED_ORIGINS = ['*']
-
-CSRF_TRUSTED_ORIGINS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True  
 
 
 # Password validation
